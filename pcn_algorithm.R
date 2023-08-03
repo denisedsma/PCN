1abe597d94d5d6a6d4ea9afeb8bdf4df15a47b2e
denisedsma <84477121+denisedsma@users.noreply.github.com>
2023-08-03 18:29:22 -0300

    Update pcn_algorithm.R

--- pcn_algorithm.R
+++ pcn_algorithm.R
@@ -1,19 +1,20 @@
 #  ALGORITMO PCN 
 #
-# - Utiliza matrizes que contem borda
-# - consertando a formula para N = n^2
-# - Ordem maxima da arvore vira argumento da função (facil testar ordens diferentes)
+# - Use matrices containing borders
+# - Fix the formula for N = n^2
+# - Maximum order of the tree becomes a function argument (easy to test different orders)
 
-#### ordem ####
-# Calcula o valor observado da vizinhaca de ordem o para o site ij
-# com argumento novo mirror para escolher se quer ou nao espelhar matriz.
-# m2 - matriz (de 2(preto) e 1(branco))
-# i - linha
-# j - coluna
-# o - ordem
-# a - alfabeto
-# n - tamanho da matriz n x n
-#ordem adaptada para lidar com agua e quando a=2!!
+#### Order ####
+# Calculate the observed value of the neighborhood of order o for the site ij
+# with the new "mirror" argument to choose whether or not to mirror the matrix.
+# m2 - matrix (of 2 (black) and 1 (white))
+# i - row
+# j - column
+# o - order
+# a - alphabet
+# n - matrix size n x n
+# Order adapted to deal with water and when a=2!!
+
 ordem <- function(m2, i, j, o, a, n, D = trunc(log(n/2))){
   l1<-m2[(D+i-o),(D+j-o):(D+j+o-1)]
   l2<-m2[(D+i+o),(D+j-o+1):(D+j+o)]
@@ -28,8 +29,10 @@ obs
 
 
 #### obs.ordem ####
-## Modifica a matriz dados de acordo com a vizinhanha observada (atualiza numero de sites e vizinhancas encontradas)
-## Atencao para o uso com alfabeto  pois ordenamos em 1,2,3....max(alfabeto)
+## Modify the data matrix based on the observed neighborhood (update the number of sites and discovered neighborhoods).
+## Pay attention to the use with the alphabet as we order it from 1 to max(alphabet).
+
+
 obs.ordem<-function(m, dados, achou, obs, o, i, j, a, n, D = trunc(log(n/2))){
   # Se ja existe esse contexto
   if( length(achou)==1){
@@ -38,7 +41,7 @@ obs.ordem<-function(m, dados, achou, obs, o, i, j, a, n, D = trunc(log(n/2))){
         dados[[o]][achou,(o*(a-1)+m[(i+D),(j+D)])] <- dados[[o]][achou,(o*(a-1)+m[(i+D),(j+D)])]+1
     }
   }
-  # Nao existe esse contexto
+  #"There is no such context."
   if(length(achou)==0){
     dados[[o]]<-rbind(dados[[o]], c(obs[1:(o*(a-1))],rep(0,(a-1)),1))
     if(m[(i+D),(j+D)]<a){
@@ -50,46 +53,55 @@ obs.ordem<-function(m, dados, achou, obs, o, i, j, a, n, D = trunc(log(n/2))){
 
 
 #### arv.amostral ####
-#Cria arvore amostral a partir de uma determinada matriz
-#adaptado para agua quando a=2
-#D = ordem maxima da arvore
+#Create a sample tree from a given matrix
+#Adapted for water when a=2
+#D = maximum order of the tree
+
 arv.amostral<-function(m, n, a, D = trunc(log(n/2))){
-  ## Entre com uma lista que sera usada de apoio
-  ## A variavel dados eh uma lista. ## Cada item da lista correspondo a uma determinada ordem de vizinhanca
-  ## Se A=3 : Para ordem de vizinhanca 2(porexemplo) dados[[2]] sera uma matrix com 9 colunas pois temos 3 cores
-  ## As 6 primeiras sao as vizinhancas de primeira, segunda ordem ## A setima e oitava correpondem ao
-  ## numero de brancos e cinzas encontrados
-  ## A nona quantas vezes observamos essa vizinhanca independente se ij eh branco cinza ou preto.
-  ## Logo se a ordem eh o e o alfabeto(nuemros de cores)=a temos para a*o+(a)
-  ##
+ ## Enter a list that will be used as support
+## The variable "dados" is a list. Each item in the list corresponds to a specific neighborhood order.
+## If A=3: For neighborhood order 2 (for example), dados[[2]] will be a matrix with 9 columns, as we have 3 colors.
+## The first 6 columns represent the first and second-order neighborhoods.
+## The seventh and eighth columns correspond to the number of white and gray colors found.
+## The ninth column represents how many times we observed this neighborhood, regardless of whether ij is white, gray, or black.
+## Therefore, if the order is o and the alphabet (number of colors) is a, we have a*o+(a) columns.
+
   dados<-NULL
-  ## Essa lista tera log(n/2) elementos= ordem maxima de vizinhanca
+  ## This list will have log(n/2) elements = maximum order of neighborhood.
+
   dados<-vector("list", (D))
   obs<-NULL
-  # Para encontrar os contexto:
-  # Calcule para o site i=j=1 o valor de todas as vizinhas e aloque na lista
+# To find the context:
+# Calculate the value of all neighbors for the site i=j=1 and allocate them in the list.
+
   for ( o in 1: D) {
     obs<-c(obs, ordem(m, 1, 1, o, a, n))
     dados[[o]]<-rbind(dados[[o]], c(obs[1:(o*(a-1))],rep(0,a)))
   }
-  #Para todos os outros sites faca o mesmo
+  #For all other sites, do the same.
+
   for(i in 1:n){
     for ( j in 1: n){
       obs<-NULL
-     #Veja se o pixel avaliado é zero (água), se for, pule para o próximo.
+     #Check if the evaluated pixel is zero (water), if it is, skip to the next one.
+
         if(m[(i+D),(j+D)]!= 0){
-            #Calcule o valor da vizinhanca para cada ordem
+           # Calculate the value of the neighborhood for each order.
+
             for ( o in 1: D){
             obs<-c(obs, ordem(m, i, j, o, a, n))
-            # Tente encontrar se ja existe essa vizinhanca
-            # Liste a quantidade de strings diferentes de ordem o (elemento o da lista)
+           # Try to find if this neighborhood already exists.
+# List the number of different strings of order o (element "o" from the list).
+
             achou<-1: dim(dados[[o]])[1]
             #achou
-            #A cada ordem fique somente com aqueles possuem a mesma vizinhanca
-            # Para que encontre um contexto deve ser testado desda raiz
+            # At each order, keep only those that have the same neighborhood.
+# To find a context, it must be tested from the root.
+
             for( p in 1:(o*(a-1))) achou<-achou[which(dados[[o]][achou,p]==obs[p])]
             #achou
-            #Atualize os dados com o contexto novo ou ja existente
+           # Update the data with the new or already existing context.
+
             dados<-obs.ordem(m, dados, achou, obs, o, i, j, a, n)
             }
           obs
@@ -111,10 +123,11 @@ arv.amostral<-function(m, n, a, D = trunc(log(n/2))){
 
 
 #### pdj ####
-#Calcula Pdj
-#as vezes (nsa[a]/ns)^nsa[a] vai pra zero e o log disso = -Inf
-#fazer nsa[a]*log(nsa[a]/ns) evita esse tipo de coisa
-#exemplo: log((844/2822)^844) = -inf, mas 844*log(844/2822) = -1018.749
+# Calculate Pdj.
+# Sometimes (nsa[a]/ns)^nsa[a] goes to zero, and the logarithm of that is -Inf.
+# Using nsa[a]*log(nsa[a]/ns) avoids such issues.
+# For example: log((844/2822)^844) = -Inf, but 844*log(844/2822) = -1018.749.
+
 pdj<-function(dados,o,s,n){
   A = 2 #alfabeto = 2
   aprod<-0
@@ -131,53 +144,64 @@ pdj<-function(dados,o,s,n){
       aprod<- aprod + nsa[a]*log(nsa[a]/ns)
     }
   }
-  aprod<- aprod - log(n^(A-1)) #n é o lado da matriz!!
+  aprod<- aprod - log(n^(A-1)) #n Ã© o lado da matriz!!
 }
 
 
 #### vdj ####
-#Calcula as indicadoras Vdj
+# Calculate the indicator Vdj.
+
 Vdj<-function(dados, n){
-  ### Calculo dos Vs e Xs ####
-  # Acrescente duas colunas
+### Calculation of Vs and Xs ###
+# Add two columns.
+
   for ( o in 1: length(dados)) dados[[o]]<-cbind(dados[[o]],0,0)
-  # Faca primeiro para ultimos filhos
+ # First, do it for the last children.
+
   u<-length(dados)
   nfilhos<-dim(dados[[u]])[1]
   for( f in 1:nfilhos) dados[[u]][f,(o+3):(o+4) ]<-c(pdj(dados,u,f,n),0)
-  # Faca para  o restante
+ # Now, do it for the rest.
+
   for( o in (u-1):1){
-    # Quantos strings?
+    # How many strings?
     nstring<-dim(dados[[o]])[1]
     for( s in 1:nstring){
       #s=1
-      # Quais sao os filhos de s
+      #What are the children of s?
+
       filhos<-1: dim(dados[[(o+1)]])[1]
       for (p in 1: o) filhos<- filhos[which(dados[[(o+1)]][filhos,p]==dados[[o]][s,p])]
-      #Qual eh o produto dos v dos filhos
+     #What is the product of the V's of the children?
+
       #vprod<-prod(dados[[(o+1)]][filhos,(o+4)])
       vprod<-sum(dados[[(o+1)]][filhos,(o+4)])
-      # Testa para encontrar V e X
-      dados[[o]][s,(o+3):(o+4)]<-c(max(pdj(dados,o,s,n),vprod),sum(vprod > pdj(dados,o,s,n))) #se true 1, se false 0
+     # Test to find V and X.
+
+      dados[[o]][s,(o+3):(o+4)]<-c(max(pdj(dados,o,s,n),vprod),sum(vprod > pdj(dados,o,s,n))) #if true 1, if false 0
     }
   }
   dados
 }
 
 #### poda ####
-#Podar arvore amostral
+# Prune the sample tree.
+
 poda<-function(arv.dados,a){
-  # Para todas as ordens
+# For all orders.
+
   for ( o in 1:(length(arv.dados)-1)){
     i=1   # auxiliar
-    #TRansformar tudo em matriz
+ # Transform everything into a matrix.
+
     if( is.vector(arv.dados[[o]]))arv.dados[[o]]<-matrix(arv.dados[[o]],nrow=1)
-    if(dim(arv.dados[[o]])[1]==0)i<-dim(arv.dados[[o]])[1]+1 ## caso teime em ser vetor
-    # Enquanto tiver vizinhanca nao analisada
+    if(dim(arv.dados[[o]])[1]==0)i<-dim(arv.dados[[o]])[1]+1 ### In case it insists on being a vector
+# While there is unanalyzed neighborhood.
+
     while (i<=dim(arv.dados[[o]])[1]){
       aux<-NULL
       if( is.vector(arv.dados[[o+1]]))arv.dados[[o+1]]<-matrix(arv.dados[[o+1]],nrow=1)
-      #olhe na coluna dos indicadores X = o(a-1)+(a-1)+ntotal+V+X
+      # Look at the column of X indicators. = o(a-1)+(a-1)+ntotal+V+X
       if (arv.dados[[o]][i,(a*o- o +a +2)]==0 && dim(arv.dados[[(o+1)]])[1]>0){
         corte<-arv.dados[[o]][i,1:(o*(a-1))]
         for(oo in (o+1):(length(arv.dados))){
